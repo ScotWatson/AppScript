5c64fe8b1158e39c36835f97f5e0132e1064765b
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-21 00:08:47 -0400

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,33 @@
+/*
+(c) 2025 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const urlPort = null;
+const resources = new Map();
+
+self.addEventlListener("fetch", (request) => {
+  const resource = resources.get(request.url);
+  if (resource) {
+    return new Response({
+      body: resource,
+    });
+  }
+  return fetch(request);
+});
+
+self.addEventListener("message", (evt) => {
+  switch (evt.data.name) {
+    case "url":
+      urlPort = evt.data.port;
+      wrlPort.add
+      break;
+    default:
+      
+  }
+  resources.set(evt.data.url)
+});
+
+function urlPortHandler(evt) {
+  
+}
